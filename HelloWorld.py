768836033095a727efe143557d917a4638ed2da5
Shan Muthukumarasamy <shanplannet@gmail.com>
2022-02-27 13:51:58 -0800

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,23 @@
+import sys
+print(sys.executable)
+print(sys.version)
+
+"""
+This is a comment
+"""
+print("Hello World")
+
+a = 2
+list1 = (1, 2, 3, 'a', 'b', 'c')
+print(list1[3])
+
+# Operators
+x = 5
+y = 2
+
+print("Multiply %d" % (x*y))
+print("Addtion %d" % (x+y))
+print("Division %0.4f" % (x/y))
+print("Floor Division %d" % (x//y))
+print("Modulus %d" % (x % y))
+print("Exponent x power y %d" % (x**y))
